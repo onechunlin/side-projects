0e7433d1cf4023980cc9fd7431b5e5a43ce83afc
onechunlin <865106110@qq.com>
2023-03-10 14:16:31 +0800

    feat: 增加会话上下文

--- packages/chatmate/lib/util.ts
+++ packages/chatmate/lib/util.ts
@@ -1,20 +1,28 @@
 import axios from "axios";
 
+type Message = {
+  role: "user" | "assistant" | "system";
+  content: string;
+};
+
 class ChatGptClient {
   private key: string;
+  private messages: Message[];
 
   constructor(apiKey: string) {
     this.key = apiKey.trim();
+    this.messages = [];
   }
 
   public async createChatCompletion(content: string): Promise<string> {
+    // 将用户发送信息放进消息队列
+    this.messages.push({ role: "user", content });
     const result = await axios
       .post(
         "https://api.openai.com/v1/chat/completions",
         {
           model: "gpt-3.5-turbo",
-          messages: [{ role: "user", content }],
-          user: this.key,
+          messages: this.messages,
         },
         {
           headers: {
@@ -25,7 +33,10 @@ class ChatGptClient {
       )
       .then((res) => res.data);
 
-    const response = result.choices[0].message.content.trim() || "出错啦！";
+    const assistantMessage = result.choices[0].message;
+    // 将助手返回信息放进消息队列
+    this.messages.push(assistantMessage);
+    const response = assistantMessage.content.trim();
 
     return response;
   }
