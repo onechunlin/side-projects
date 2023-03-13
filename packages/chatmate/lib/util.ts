1bc6f91085b42b471d3b5896743cbf72bf076f5d
onechunlin <865106110@qq.com>
2023-03-13 16:50:25 +0800

    perf: chatmate 改为 Stream 输出

--- packages/chatmate/lib/util.ts
+++ packages/chatmate/lib/util.ts
@@ -1,9 +1,63 @@
 import axios from "axios";
+import chalk from "chalk";
+import ora from "ora";
+import { Transform, TransformCallback } from "stream";
 
 type Message = {
   role: "user" | "assistant" | "system";
   content: string;
 };
+
+type Callback = (content: string) => void;
+
+const spinner = ora({
+  prefixText: chalk.gray("\n请求数据中，请稍后"),
+  spinner: "soccerHeader",
+});
+
+class DataTransform extends Transform {
+  private done: Callback;
+  private content: string;
+
+  constructor(cb: Callback) {
+    super();
+    this.done = cb;
+    this.content = "";
+  }
+
+  _transform(
+    chunk: any,
+    encoding: BufferEncoding,
+    callback: TransformCallback
+  ) {
+    const chunkStr = chunk.toString().trim();
+    const dataArr = chunkStr.split("\n\n");
+
+    // 一个 chunk 里可能有多个 data 事件内容，所以在这里需要进行特殊处理
+    dataArr.forEach((item: string) => {
+      const data = item.replace("data: ", "");
+      if (data === "[DONE]") {
+        return;
+      }
+      // 转成对象，拿到具体的内容
+      const dataObj = safeParse(data);
+      const msg: string = dataObj?.choices?.[0]?.delta?.content;
+      if (msg) {
+        // 如果消息开头有空白符，则去掉
+        const formatMsg = !this.content ? msg.trimStart() : msg;
+        this.content += formatMsg;
+        this.push(chalk.yellow(formatMsg));
+      }
+    });
+    callback();
+  }
+
+  _flush(callback: TransformCallback) {
+    this.done(this.content);
+    this.push("\n\n");
+    callback();
+  }
+}
 
 class ChatGptClient {
   private key: string;
@@ -14,36 +68,68 @@ class ChatGptClient {
     this.messages = [];
   }
 
-  public async createChatCompletion(content: string): Promise<string> {
+  public async createChatCompletion(content: string): Promise<void> {
     // 将用户发送信息放进消息队列
     this.messages.push({ role: "user", content });
+    spinner.start();
     const result = await axios
       .post(
         "https://api.openai.com/v1/chat/completions",
         {
           model: "gpt-3.5-turbo",
           messages: this.messages,
+          stream: true,
         },
         {
           headers: {
             Authorization: `Bearer ${this.key}`,
           },
-          timeout: 30000,
+          responseType: "stream",
         }
       )
-      .then((res) => res.data);
+      .then((res) => res.data)
+      .finally(() => {
+        spinner.stop();
+      });
 
-    const assistantMessage = result.choices[0].message;
-    // 将助手返回信息放进消息队列
-    this.messages.push(assistantMessage);
-    const response = assistantMessage.content.trim();
+    console.log(`\n${chalk.bold.blue("Question: ")}${chalk.gray(content)}`);
+    console.log(chalk.bold.green("ChatGPT: "));
 
-    return response;
+    return new Promise((resolve) => {
+      const dataTransform = new DataTransform((message: string) => {
+        // 将助手返回信息放进消息队列
+        this.messages.push({
+          role: "assistant",
+          content: message,
+        });
+        resolve();
+      });
+      result.pipe(dataTransform).pipe(process.stdout);
+    });
   }
 
+  // 通过发送一个消息来测试 open key 是否有效
   public async checkAuth(): Promise<boolean> {
     try {
-      await this.createChatCompletion("hi");
+      await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: "hi",
+            },
+          ],
+          stream: true,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.key}`,
+          },
+          responseType: "stream",
+        }
+      );
       return true;
     } catch (error) {
       return false;
@@ -51,4 +137,12 @@ class ChatGptClient {
   }
 }
 
-export { ChatGptClient };
+function safeParse(str: string): Record<string, any> {
+  let obj: Record<string, any> = {};
+  try {
+    obj = JSON.parse(str);
+  } catch (error) {}
+  return obj;
+}
+
+export { ChatGptClient, safeParse };
